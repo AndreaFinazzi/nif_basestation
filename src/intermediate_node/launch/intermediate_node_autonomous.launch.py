1309b2eed683ed1c8d55b70fedd0c08e5316721f
Alexander Wischnewski <alexander.wischnewski@tum.de>
2021-06-30 16:42:12 -0400

    autonomous launch file for intermediate node and modified launch file

---
+++ src/intermediate_node/launch/intermediate_node_autonomous.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+
+    intermediate = launch_ros.actions.Node(
+        package = 'intermediate_node',
+        executable = 'intermediate_node',
+        output = 'screen', 
+        remappings=[
+            ("/joystick/steering_cmd", "/joystick/steering_cmd_not_used")
+        ]
+    )
+
+    return LaunchDescription([intermediate])
