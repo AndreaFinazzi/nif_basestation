86c083dc35d24f230a01a451cc87906a8ede621e
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-07-19 10:35:08 -0400

    Rest API on zenoh

--- operations/scripts/bst_fm.sh
+++ operations/scripts/bst_fm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This is my guess at what the correct args are lol
-ROS_DOMAIN_ID=2 RUST_LOG=debug zenoh-bridge-dds -d 2 -m peer -e tcp/10.42.0.200:7447 -s /DO12 -r /DO12/** -w /DO12/** \
+ROS_DOMAIN_ID=2 RUST_LOG=debug zenoh-bridge-dds -d 2 -m peer -e tcp/10.42.0.200:7447 -s /DO12 -r /DO12/** -w /DO12/**  --rest-plugin \
 --allow "ros_discovery_info|diagnostics|/joystick/.*|rt/lateral_error|lookahead_error|ct_report|misc_report_do|pt_report|/vehicle/.*|rc_to_ct|/raptor_dbw_interface/pt_report"
