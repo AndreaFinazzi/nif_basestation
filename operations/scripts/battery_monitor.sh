19d995f75140ea11f01d97b1b51a259b2918cd84
Alexander <alexander.wischnewski@tum.de>
2021-07-26 15:04:23 -0400

    increase voltage monitoring level to 12.7

--- operations/scripts/battery_monitor.sh
+++ operations/scripts/battery_monitor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Started battery monitoring script"
-if ros2 topic echo /raptor_dbw_interface/misc_report_do | grep -m 1 "battery_voltage: 12[0-6]"; then
+if ros2 topic echo /raptor_dbw_interface/misc_report_do | grep -m 1 "battery_voltage: 12[0-7]"; then
     while true
     do
 	  spd-say "Battery low"
