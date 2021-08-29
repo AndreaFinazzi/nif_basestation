14b7ec8076f7203bd792e1085d9ccaccc06361ca
Andrew Saba <asaba96@pitt.edu>
2021-08-29 17:46:25 -0400

    added remote sensors bagging; misc. cleanup

---
+++ operations/scripts/remote_sensors_bagging.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh user@10.42.4.200 tmuxp load /home/user/VehicleConfig/start_sensors_bagging.yaml
---
+++ operations/scripts/remote_sensors_kill.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh user@10.42.4.200 tmux kill-session -t sensors_bagging_launch
